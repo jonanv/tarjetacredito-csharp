bdb6c98b27055284b411f4e77f4c70afde021b3d
Giovanni Vargas <jonan-vargas23@hotmail.com>
2021-03-06 21:56:55 -0500

    Se crea el modelo TarjetaCredito.cs

---
+++ tarjetacredito-csharp/Models/TarjetaCredito.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace tarjetacredito_csharp.Models
+{
+    public class TarjetaCredito
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Titular { get; set; }
+
+        [Required]
+        public string NumeroTarjeta { get; set; }
+
+        [Required]
+        public string FechaExpiracion { get; set; }
+
+        [Required]
+        public string CVV { get; set; }
+    }
+}
