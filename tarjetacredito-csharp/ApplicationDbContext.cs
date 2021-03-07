a9c3e3b91429d243e51f510351c6645ca649998d
Giovanni Vargas <jonan-vargas23@hotmail.com>
2021-03-06 22:07:46 -0500

    Se crea el ApplicationDbContext.cs para la conexion de la base de datos y se configura en el Startup.cs y appsettings.cs

---
+++ tarjetacredito-csharp/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using tarjetacredito_csharp.Models;
+
+namespace tarjetacredito_csharp
+{
+    public class ApplicationDbContext: DbContext
+    {
+        DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        {
+        }
+    }
+}
